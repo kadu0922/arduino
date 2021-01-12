93f8c2c794e1fc9d1e554ad96984fffd3ecbf9ab
Nakanishi <w.app0922@gmail.com>
2021-01-12 20:21:06 +0900

    修正

--- server/index.php
+++ server/index.php
@@ -1,8 +1,10 @@
 <?php
+$jsonPath = "/home/www-data/log/data.json";
 if(isset($_POST)){
-    foreach($_POST as $Key => $Value) $jsonstr =  json_encode($_POST);
-    $fp = fopen("/home/www-data/log/data.json", "a");
-    fwrite($fp,$jsonstr);
+    $jsonstr = json_decode(file_get_contents($jsonPath),true);
+    $fp = fopen($jsonPath, "w");
+    $jsonstr[]=$_POST;
+    fwrite($fp,json_encode($jsonstr));
     fclose($fp);
 }
 ?>
