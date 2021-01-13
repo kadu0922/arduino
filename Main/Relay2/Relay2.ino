36e49550a646b7ff4cb30909a6973f97d946acfd
kadu0922 <w.app0922@gmail.com>
2021-01-13 18:21:50 +0900

    Relay2バグ取り

--- Main/Relay2/Relay2.ino
+++ Main/Relay2/Relay2.ino
@@ -27,8 +27,30 @@ SoftwareSerial LoraSerial(LORA_RX, LORA_TX);
 void setResetRtc(){
     Wire.begin(); // arudinoをマスターとして接続
     delay(1000);  // 発振子の動作待機
+    
+    Wire.beginTransmission(RTCaddress); //接続するIC2のモジュールを選択
     Wire.write(0x00);                   // データを転送するレジスタ番号を指定
     Wire.write(0b00100000);                   // 00 Control 1　STOP = 1 動作停止
+    Wire.write(0x00);                         // 01 Control 2 
+    Wire.write(0x00);                         // 02 Seconds　
+    Wire.write(0x00);                   // 03 Minutes
+    Wire.write(0x00);                   // 04 Hours
+    Wire.write(0x00);                   // 05 Days
+    Wire.write(0x00);                   // 06 Weekdays
+    Wire.write(0x00);                   // 07 Months
+    Wire.write(0x00);                   // 08 Years
+    //20年1月１日00:00に設定
+
+    //Alram レジスタ
+    Wire.write(0x00);       // 09 Minutes Alarm　
+    Wire.write(0x00);       // 0A Hours Alarm
+    Wire.write(0x00);       // 0B Days Alarm
+    Wire.write(0x00);       // 0C Weekdays Alarm
+
+    //timerレジスタ
+    Wire.write(0x00);       // 0D CLKOUT
+    Wire.write(0x00);       // 0E TimerControl
+    Wire.write(0x00);       // 0F Timer 7秒設定
 }
 
 /* Sleep用RTCの設定を初期化する関数 */
@@ -240,7 +262,10 @@ void setup()
     delay(1500);
 
     LoraSerial.readStringUntil(10); //OKの文字列を読み飛ばす
+    delay(1500);
+    LoraSerial.readStringUntil(10); //OKの文字列を読み飛ばす
     LoraSerial.flush();
+
     setSystemInit();
 }
 
