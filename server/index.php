a78ab5f7806d79c8554737602a95082a9b642f66
kadu0922 <w.app0922@gmail.com>
2021-01-14 14:58:22 +0900

    温度、湿度表示

--- server/index.php
+++ server/index.php
@@ -3,6 +3,9 @@ $jsonPath = "/home/www-data/log/data.json";
 if(isset($_POST)){
     $jsonstr = json_decode(file_get_contents($jsonPath),true);
     $fp = fopen($jsonPath, "w");
+    $_POST["T"] = -45+175*hexdec($_POST["T"])/65535;
+    $_POST["H"] = 100*hexdec($_POSaT["H"])/65535;
+    $_POST["V"] = 3.0/4095*hexdec($_POST["V"]);
     $jsonstr[]=$_POST;
     fwrite($fp,json_encode($jsonstr));
     fclose($fp);
