c7882e9880c1871b2633786b9b3091b84bc170b1
kadu0922 <w.app0922@gmail.com>
2021-01-29 15:03:39 +0900

    realy2 修正　relay3完成

--- Main/Relay1/Relay1.ino
+++ Main/Relay1/Relay1.ino
@@ -42,36 +42,6 @@ void Set_Bootstate(int state) {
     EEPROM.write(0, state);
 }
 
-/* rtcの動作停止用 */
-void setResetRtc(){
-    Wire.begin(); // arudinoをマスターとして接続
-    delay(1000);  // 発振子の動作待機
-    
-    Wire.beginTransmission(RTCaddress); //接続するIC2のモジュールを選択
-    Wire.write(0x00);                   // データを転送するレジスタ番号を指定
-    Wire.write(0b00100000);                   // 00 Control 1　STOP = 1 動作停止
-    Wire.write(0x00);                         // 01 Control 2 
-    Wire.write(0x00);                         // 02 Seconds　
-    Wire.write(0x00);                   // 03 Minutes
-    Wire.write(0x00);                   // 04 Hours
-    Wire.write(0x00);                   // 05 Days
-    Wire.write(0x00);                   // 06 Weekdays
-    Wire.write(0x00);                   // 07 Months
-    Wire.write(0x00);                   // 08 Years
-    //20年1月１日00:00に設定
-
-    //Alram レジスタ
-    Wire.write(0x00);       // 09 Minutes Alarm　
-    Wire.write(0x00);       // 0A Hours Alarm
-    Wire.write(0x00);       // 0B Days Alarm
-    Wire.write(0x00);       // 0C Weekdays Alarm
-
-    //timerレジスタ
-    Wire.write(0x00);       // 0D CLKOUT
-    Wire.write(0x00);       // 0E TimerControl
-    Wire.write(0x00);       // 0F Timer 7秒設定
-}
-
 /* Sleep用RTCの設定を初期化する関数 */
 void setSleepRtcConfig(){
     RTC_FLAG = false;
@@ -232,7 +202,7 @@ void setReadSendLoraData(){
 
     while(!PACKET_FLAG){
         delay(10);
-        if (2 == -1){
+        if (LoraSerial.read() != -1){
             PACKET_FLAG = true; //キャプチャ成功
             Data = LoraSerial.readStringUntil('\r');//ラインフィードまで格納する
             clearBuffer();
@@ -262,7 +232,6 @@ void setSystemInit(){
 /* Main関数 */
 void setup()
 {
-    
     pinMode(SLEEP_PIN,OUTPUT);         //Loraのスリープピン初期化
     digitalWrite(SLEEP_PIN, LOW);      //Low = active_mode　High = sleep_mode
 
