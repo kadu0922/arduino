73920112f614aea078b700f1378bcd7f2177a9be
kadu0922 <w.app0922@gmail.com>
2021-01-29 15:03:39 +0900

    realy1 修正　relay2完成

--- Main/Relay2/Relay2.ino
+++ Main/Relay2/Relay2.ino
@@ -14,7 +14,7 @@
 #define LORA_RX 4       /* Software_RX_4 */
 #define LORA_TX 5       /* Software_TX_5 */
 #define CMDDELAY 100    /* CMD待機時間 */
-#define BOOTDELAY 1500  /* Boot待機時間 */
+#define BOOTDELAY 1000  /* Boot待機時間 */
 
 #define BAUTRATE 9600   /* BautRate */
 
@@ -40,36 +40,6 @@ byte Load_Bootstate() {
 
 void Set_Bootstate(int state) {
     EEPROM.write(0, state);
-}
-
-/* rtcの動作停止用 */
-void setResetRtc(){
-    Wire.begin(); // arudinoをマスターとして接続
-    delay(1000);  // 発振子の動作待機
-    
-    Wire.beginTransmission(RTCaddress); //接続するIC2のモジュールを選択
-    Wire.write(0x00);                   // データを転送するレジスタ番号を指定
-    Wire.write(0b00100000);                   // 00 Control 1　STOP = 1 動作停止
-    Wire.write(0x00);                         // 01 Control 2 
-    Wire.write(0x00);                         // 02 Seconds　
-    Wire.write(0x00);                   // 03 Minutes
-    Wire.write(0x00);                   // 04 Hours
-    Wire.write(0x00);                   // 05 Days
-    Wire.write(0x00);                   // 06 Weekdays
-    Wire.write(0x00);                   // 07 Months
-    Wire.write(0x00);                   // 08 Years
-    //20年1月１日00:00に設定
-
-    //Alram レジスタ
-    Wire.write(0x00);       // 09 Minutes Alarm　
-    Wire.write(0x00);       // 0A Hours Alarm
-    Wire.write(0x00);       // 0B Days Alarm
-    Wire.write(0x00);       // 0C Weekdays Alarm
-
-    //timerレジスタ
-    Wire.write(0x00);       // 0D CLKOUT
-    Wire.write(0x00);       // 0E TimerControl
-    Wire.write(0x00);       // 0F Timer 7秒設定
 }
 
 /* Sleep用RTCの設定を初期化する関数 */
@@ -265,8 +235,6 @@ void setup()
     pinMode(SLEEP_PIN,OUTPUT);         //Loraのスリープピン初期化
     digitalWrite(SLEEP_PIN, LOW);      //Low = active_mode　High = sleep_mode
 
-    setResetRtc();
-
     pinMode(LED, OUTPUT);                   //13を出力設定(LED用)
     
     pinMode(INPIN, INPUT_PULLUP);           //2番をプルアップ設定
@@ -275,9 +243,11 @@ void setup()
                                             // FALLING ピンの状態が HIGH → LOW になった時に割り込み
     Serial.begin(9600);                     //siralの速度
 
+    setRestartLora();
+    delay(BOOTDELAY);
     if (Load_Bootstate() == 0) { //メモリに0が書いてあるので再起動する
         Set_Bootstate(1); //EEPROMに1を書いておく　次は通常起動
-        delay(500);
+        delay(BOOTDELAY);
         Serial.println("Rebooting");
         software_reset();
     }
@@ -285,9 +255,8 @@ void setup()
 
     Serial.print("Lora2\n---------------------------\n");
 
-    setRestartLora();
     setLoraInit();
-    delay(1500);
+    delay(CMDDELAY);
 
     LoraSerial.readStringUntil(10); //OKの文字列を読み飛ばす
     LoraSerial.flush();
